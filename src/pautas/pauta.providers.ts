7c76cfbc1352d8a9cf8898b28939278d44bbae5b
Danubio de Araujo <danubio.bwm@gmail.com>
2024-03-05 19:48:34 -0300

    pauta service

---
+++ src/pautas/pauta.providers.ts
@@ -0,0 +1,12 @@
+import {DataSource, Repository} from "typeorm";
+import {Pauta} from "./pauta.entity";
+import { Provider } from "@nestjs/common"
+
+const pautaRepository: Provider <Repository<Pauta>>={
+  provide: 'PAUTA_REPOSITORY',
+  useFactory: (dataSource: DataSource) => dataSource.getRepository(Pauta),
+  inject: ['DATA_SOURCE'],
+}
+
+
+export const pautasProviders:Provider[] = [pautaRepository]
--- src/pautas/pautas.module.ts
+++ src/pautas/pautas.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { PautasService } from './pautas.service';
+import {pautasProviders} from './pauta.providers';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [PautasService, ...pautasProviders],
 })
 export class PautasModule {}
